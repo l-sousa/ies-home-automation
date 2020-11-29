7fad9c86416e4512fb0de2f48e5e34e8b208daf5
Duarte Mortágua <duarte.ntm@ua.pt>
2020-11-29 22:22:42 +0000

    Initial mvn project with simple spring login logic

---
+++ src/main/java/com/homemonitor/homemonitor/HomemonitorApplication.java
@@ -0,0 +1,13 @@
+package com.homemonitor.homemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homemonitor/homemonitor/HomemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homemonitor.homemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
