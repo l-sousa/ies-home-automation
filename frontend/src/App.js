fbb2b09507d830664a04299ee803d3df83f6bc94
l-sousa <joselmdbsousa@ua.pt>
2020-12-05 17:29:03 +0000

    major refactor, added folders to backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-streaming';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Line
+      data={{
+        datasets: [{
+          data: [],
+          label: 'Dataset 1',
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          lineTension: 0,
+          borderDash: [8, 4]
+        }, {
+          data: [],
+          label: 'Dataset 2',
+          borderColor: 'rgb(54, 162, 235)',
+          backgroundColor: 'rgba(54, 162, 235, 0.5)'
+        }]
+      }}
+      options={{
+        scales: {
+          xAxes: [{
+            type: 'realtime',
+            realtime: {
+              onRefresh: function(chart) {
+                chart.data.datasets.forEach(function(dataset) {
+                  dataset.data.push({
+                    x: Date.now(),
+                    y: Math.random()
+                  });
+                });
+              },
+              delay: 5000,
+              frameRate: 60,
+              duration: 43200000
+            }
+          }]
+        }
+      }}
+    />
+    );
+  }
+}
+
+
+export default App;
