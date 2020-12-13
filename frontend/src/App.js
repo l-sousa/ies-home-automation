f637e033d913a7a43766e05785b0ee69b81ded06
Duarte Mortágua <duarte.ntm@ua.pt>
2020-12-13 18:27:58 +0000

    Updated graphs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,52 @@ import 'chartjs-plugin-streaming';
 import './App.css';
 
 class App extends Component {
-
-    get_last_data() {
-        var axios = require('axios');
-
-        var config = {
-            method: 'get',
-            url: '/api/values/1/last/1'
-        };
-
-        axios(config)
-            .then(function(response) {
-                console.log(JSON.stringify(response.data));
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: []
     }
+  }
+
+  componentDidMount() {
+      var axios = require('axios');
+
+      var config = {
+          method: 'get',
+          url: '/api/values/2/period/6'
+      };
+
+      axios(config)
+          .then( response => {
+              const data_array = [];
+              response.data.forEach(item => data_array.push({x:Date.parse(item.ts), y:item.value}));
+              return data_array;
+          })
+          .then( data => {
+            this.setState({ chartData: data });
+            //console.log(data);
+          })
+          .catch(function(error) {
+              console.log(error);
+          });
+    }
+    
     render() {
+      const { chartData } = this.state;
+      console.log(chartData);
         return ( <
             Line data = {
                 {
                     datasets: [{
-                        data: this.get_last_data(),
+                        data: [],
                         label: 'Dataset 1',
                         borderColor: 'rgb(255, 99, 132)',
                         backgroundColor: 'rgba(255, 99, 132, 0.5)',
                         lineTension: 0,
                         borderDash: [8, 4]
                     }, {
-                        data: [],
+                        data: chartData,
                         label: 'Dataset 2',
                         borderColor: 'rgb(54, 162, 235)',
                         backgroundColor: 'rgba(54, 162, 235, 0.5)'
@@ -50,13 +66,13 @@ class App extends Component {
                                     chart.data.datasets.forEach(function(dataset) {
                                         dataset.data.push({
                                             x: Date.now(),
-                                            y: Math.random()
+                                            y: 68
                                         });
                                     });
                                 },
                                 delay: 5000,
                                 frameRate: 60,
-                                duration: 43200000
+                                duration: 21600000
                             }
                         }]
                     }
